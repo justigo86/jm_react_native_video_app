900b6233075d3ea19364d8e8a6f72529abea2fa6
Otto <justigo86@gmail.com>
2024-11-13 12:49:11 -0500

    initial setup for signin screen

--- app/(auth)/signin.tsx
+++ app/(auth)/signin.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[90vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-white text-2xl font-psemibold mt-10 text-semibold">
+            SignIn
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
